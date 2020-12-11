32405100d62cb25e87bd5d13cccc0f1737a90d65
石工 <chenyan.wcy@koubei.com>
2020-12-11 16:44:33 +0800

    feat: 增加优先级

--- arch-annotation/src/main/java/me/wcy/arch/annotation/AbsModule.java
+++ arch-annotation/src/main/java/me/wcy/arch/annotation/AbsModule.java
@@ -6,9 +6,11 @@ import org.jetbrains.annotations.NotNull;
  * Created by wcy on 2020/12/10.
  */
 public abstract class AbsModule implements Comparable<AbsModule> {
-    public final static int MAX_PRIORITY = Integer.MIN_VALUE;
+    public final static int MAX_PRIORITY = Integer.MAX_VALUE;
+    public final static int HIGH_PRIORITY = 1000;
     public final static int NORM_PRIORITY = 0;
-    public final static int MIN_PRIORITY = Integer.MAX_VALUE;
+    public final static int LOW_PRIORITY = -1000;
+    public final static int MIN_PRIORITY = Integer.MIN_VALUE;
 
     public AbsModule() {
     }
@@ -31,6 +33,6 @@ public abstract class AbsModule implements Comparable<AbsModule> {
 
     @Override
     public int compareTo(@NotNull AbsModule absModule) {
-        return Integer.compare(getPriority(), absModule.getPriority());
+        return -Integer.compare(getPriority(), absModule.getPriority());
     }
 }
